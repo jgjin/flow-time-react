8b13257306fbd7133701a7c083c8db1eba18631e
Jason Jin <jgjin@umich.edu>
2024-01-06 00:19:45 -0500

    Outlines app

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useCallback, useEffect, useState } from "react"
+
+import "./App.css"
+import StatusMessage from "./components/StatusMessage"
+import Timer from "./components/Timer"
+import { type CurrentMode, Mode, toggleCurrentMode } from "./lib/mode"
+import { type CurrentStatus, getCurrentStatus } from "./lib/status"
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [currentMode, setCurrentMode] = useState<CurrentMode>({
+        mode: Mode.Working,
+        started: new Date(),
+    })
+    const [currentStatus, setCurrentStatus] = useState<CurrentStatus>(
+        getCurrentStatus(currentMode)
+    )
+
+    const toggleMode = useCallback(() => {
+        const newMode = toggleCurrentMode(currentMode)
+        const newStatus = getCurrentStatus(newMode)
+
+        setCurrentMode(newMode)
+        setCurrentStatus(newStatus)
+    }, [currentMode, setCurrentMode, setCurrentStatus])
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            const newStatus = getCurrentStatus(currentMode)
+            setCurrentStatus(newStatus)
+        }, 500)
+
+        return () => { clearInterval(interval) }
+    }, [currentMode, setCurrentStatus])
+
+    return (
+        <div className="App" onClick={toggleMode}>
+            <Timer seconds={currentStatus.seconds} />
+            <StatusMessage status={currentStatus.status} />
+        </div>
+    )
 }
 
-export default App;
+export default App
